202d8c94199d948931b166a21769b5f753be7b6d
HanneThienpondt <Hanne.Thienpondt@outlook.com>
2024-09-25 21:24:22 +0200

    Round dgbdrift0drho

--- STELLA_CODE/geometry/geometry_miller.f90
+++ STELLA_CODE/geometry/geometry_miller.f90
@@ -790,7 +790,7 @@ contains
       
       ! The <dgbdrift0drho> variable differs on macos-14 (CMake) with respect to the other operating systems
       do i = -nz, nz
-         dgbdrift0drho(i) = round(dgbdrift0drho(i), 11) 
+         dgbdrift0drho(i) = round(dgbdrift0drho(i), 10) 
       end do
 
       
